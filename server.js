787cf1a76a24bb89f67763d433a88d0750e9a2f9
Eyob-second <richardhalllfz361@gmail.com>
2026-03-22 07:10:21 +0300

    install commitment

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require('path');
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Sends message to all users
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
